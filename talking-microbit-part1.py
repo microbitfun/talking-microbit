f69aba502ef2ea82877743dedf6574f3c082ca2c
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-16 08:32:47 +0200

    Add files via upload

---
+++ talking-microbit-part1.py
@@ -0,0 +1,15 @@
+# Add your Python code here. E.g.
+from microbit import *
+import music
+import speech
+
+while True:
+    display.scroll('Hello')
+    display.show(Image.HEART)
+    sleep(2000)
+    music.play(music.PYTHON)
+    sleep(2000)
+    display.show(Image.HEART_SMALL)
+    speech.say("Learn to Invent,")
+    speech.say("with the microbit!")
+    sleep(2000)
